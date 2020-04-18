f0a6dda25f7d54bd237124778dd5d466de1635a4
Ruba Sbeih <rsasbeih@gmail.com>
2020-04-18 23:21:30 +0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { GiftCardsComponent } from './gift-cards/gift-cards.component';
+import { AccountComponent } from './account/account.component';
+import { ReservationsComponent } from './reservations/reservations.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',component: HomeComponent},
+  {path: 'profile',component: ProfileComponent},
+  {path: 'gift-cards',component: GiftCardsComponent},
+  {path: 'account',component: AccountComponent},
+  {path: 'reservations',component: ReservationsComponent},
+  {path: 'contact_us',component: ContactUsComponent},
+  {path: 'about_us',component: AboutUsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import { BusinessProfileComponent } from './business-profile/business-profile.component';
+import { ReservationsComponent } from './reservations/reservations.component';
+import { AccountComponent } from './account/account.component';
+import { GiftCardsComponent } from './gift-cards/gift-cards.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ProfileComponent,
+    BusinessProfileComponent,
+    ReservationsComponent,
+    AccountComponent,
+    GiftCardsComponent,
+    AboutUsComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
